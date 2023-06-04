1dec502045851eea45ac4e4112fd28b6015ded9c
argyaWicaksana <argya7@gmail.com>
2023-06-04 10:12:48 +0700

    add view for auth and dashboard

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,28 @@
+<aside id="sidebar-wrapper">
+    <div class="sidebar-brand">
+        <a href="index.html">Stisla</a>
+    </div>
+    <div class="sidebar-brand sidebar-brand-sm">
+        <a href="index.html">St</a>
+    </div>
+    <ul class="sidebar-menu">
+        <li class="nav-link">
+            <a href="#">
+                <i class="fas fa-tachometer-alt"></i>
+                <span>Dashboard</span>
+            </a>
+        </li>
+        <li class="nav-link">
+            <a href="#">
+                <i class="fas fa-trophy"></i>
+                <span>Lomba</span>
+            </a>
+        </li>
+        <li class="nav-link">
+            <a href="#">
+                <i class="far fa-file-alt"></i>
+                <span>Sertifikat</span>
+            </a>
+        </li>
+    </ul>
+</aside>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Auth
+Route::get('login', fn() => view('auth.login'))->name('login');
+Route::get('register', fn() => view('auth.register'))->name('register');
+
+// Dashboard
+Route::prefix('dashboard')->name('dashboard.')->group(function () {
+    Route::get('/', fn() => view('dashboard.index'))->name('index');
+});
